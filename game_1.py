cc904f163cffee4e2bcbe55f38cc84c641d22c0a
Christopher Watson <cwatson113@outlook.com>
2015-01-28 18:59:10 -0800

    initial framework complete

---
+++ game_1.py
@@ -0,0 +1,93 @@
+def entrance():
+    '''This is the initial room the player will begin their adventure.'''
+    pass
+
+def orange_rm_1():
+    '''Todo:
+
+    red key(hidden in desk drawer)
+    health(desk top)
+    desk
+    rat (24% damage)
+    red door
+    '''
+    pass
+
+def red_rm_2():
+    '''locked entrance- requires red key
+
+    to do:
+    skull of meatboy (20 questions)
+    bamboo (blowgun)
+    desk
+    dr. fetus (1% dmg atk, runs away after attack. mentions hidden passage)
+    '''
+    pass
+
+def blue_rm_1():
+    '''to do list:
+
+    book case (hidden passage)
+    health (25%)
+    shield
+    '''
+    pass
+
+def blue_rm_2():
+    '''to do list:
+
+    axe for boss fight
+    red key (bowl)
+    locked orange door (to main boss)
+    '''
+    pass
+
+def red_rm_1():
+    '''to do list:
+
+    door into room is locked (orange key hidden in red_rm_1)
+    note from file
+    dart
+    bread (inv - must use)
+    '''
+    pass
+
+def red_rm_3():
+    '''to do list:
+
+    stick (weapon)
+    health (25%)
+    '''
+    pass
+
+def red_rm_4():
+    '''banquet hall
+
+    table
+    food (invokes rat to attack if eaten/taken)
+    rat (24% dmg atk)
+    chair
+    '''
+    pass
+
+def giant_toddler():
+    '''nursery atmosphere
+
+    to do:
+    giant toddler
+    rattle
+    boss room key
+    '''
+    pass
+
+def watermelon_rex():
+    '''den of whoever runs this mad house
+
+    pink key
+    health
+    animal heads on walls (interactive - one head breaks upon engaging boss)
+    watermelon_rex
+    fire place
+    giant mahogany desk
+    '''
+    pass
